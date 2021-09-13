6463b2f4b4d1928520278cef25aa6263bcb83563
조현지 <69295697+JoHyeonJi0408@users.noreply.github.com>
2021-09-14 02:49:14 +0900

    월_부족한 금액 계산하기

---
+++ src/1주차/월_부족한 금액 계산하기/Bela.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long answer = 0;
+    long sum = 0;
+
+    for (int i = 1; i <= count; i++) {
+        sum += i;
+    }
+
+    sum *= price;
+    answer = sum - money;
+
+    return answer >= 0 ? answer : 0;
+}
